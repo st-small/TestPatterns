6cafd4f2e05341992a1ec6a1c3865f4c5b0a0039
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-11-04 15:09:35 +0200

    initial commit

---
+++ TestPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPatterns
+//
+//  Created by Stanly Shiyanovskiy on 04.11.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
